3af3ea7cd4b80c7fc1da5dc45f67ac78506ffa8d
Deanb1996 <39913308+Deanb1996@users.noreply.github.com>
2019-04-06 13:36:39 +0100

    Player inputs

--- FireCat/Assets/Scripts/PlayerController.cs
+++ FireCat/Assets/Scripts/PlayerController.cs
@@ -5,31 +5,35 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float playerSpeed;
-
-    private CharacterController character;
+    public float playerRotationSpeed;
+    public int playerNumber;
 
     private float xAxis;
     private float yAxis;
+    private float xAxisAim;
+    private float yAxisAim;
 
 	// Use this for initialization
 	void Start ()
     {
-        character = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        xAxis = Input.GetAxisRaw("Horizontal");
-        yAxis = Input.GetAxisRaw("Vertical");
+        xAxis = Input.GetAxisRaw("Horizontal" + playerNumber.ToString());
+        yAxis = Input.GetAxisRaw("Vertical" + playerNumber.ToString());
 
-        if(!(xAxis == 0 && yAxis == 0))
-        {
+        xAxisAim = Input.GetAxisRaw("HorizontalAim" + playerNumber.ToString());
+        yAxisAim = Input.GetAxisRaw("VerticalAim" + playerNumber.ToString());
+
         //Move and rotate based on joystick input
-        Vector3 movement = new Vector3(xAxis, 0, yAxis) * playerSpeed * Time.deltaTime;
-        Vector3 lookDir = new Vector3(movement.x, 0, movement.z);
-        transform.rotation = Quaternion.LookRotation(lookDir);
-        character.Move(movement);
-        }
+        Vector3 movement = new Vector3(xAxis, 0, -yAxis) * playerSpeed * Time.deltaTime;
+        transform.position += movement;
+
+        Vector3 lookDir = new Vector3(xAxisAim, 0, -yAxisAim).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(lookDir);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, playerRotationSpeed * Time.deltaTime);
+
     }
 }
