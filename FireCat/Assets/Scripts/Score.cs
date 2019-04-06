ca97a9c85a958b422be41f17e8388cdaa4c08dd3
Anthony Beacock <tonybeacock@hotmail.co.uk>
2019-04-06 13:06:14 +0100

    push

---
+++ FireCat/Assets/Scripts/Score.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public List<Text> listOfPlayerScoreTexts;
+    List<int> listOfPlayerScores;
+    int numberOfPlayers;
+
+    // Use this for initialization
+    void Start()
+    {
+        listOfPlayerScores = new List<int>(listOfPlayerScoreTexts.Capacity);
+        numberOfPlayers = listOfPlayerScoreTexts.Capacity;
+
+        // Initialise the text objects on the screen
+        for (int i = 0; i < numberOfPlayers; i++)
+        {
+            listOfPlayerScoreTexts[i].text = "0";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Updates the text objects on the screen with integer score values
+        for (int i = 0; i < numberOfPlayers; i++)
+        {
+            listOfPlayerScoreTexts[i].text = listOfPlayerScores[i].ToString();
+        }
+    }
+}
