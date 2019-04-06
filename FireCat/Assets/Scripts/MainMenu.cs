9f4500594cb6ce8ffadb247958a0080299652e44
Deanb1996 <39913308+Deanb1996@users.noreply.github.com>
2019-04-06 18:22:45 +0100

    Controller inputs for main menu

---
+++ FireCat/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetButton("Start1") || Input.GetButton("Start2") || Input.GetButton("Start3") || Input.GetButton("Start4"))
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+
+        if(Input.GetButtonDown("Cancel"))
+        {
+            Application.Quit();
+        }
+    }
+}
